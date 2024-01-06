909c12c2dd537cdbabeccc6600ae7fa44e2b2760
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2024-01-06 16:45:54 -0500

    added analyitics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import Nav from "./Nav";
 import Contact from "./Contact";
@@ -6,8 +5,10 @@ import Head from "./Head";
 import About from "./About";
 import Services from "./Services";
 import Footer from "./Footer";
+import { inject } from "@vercel/analytics";
 
 function App() {
+  inject();
   return (
     <>
       <div className="bg-white">
